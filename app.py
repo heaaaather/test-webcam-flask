b0a3b18ee1f19a27f001531bd3284bc3de4da445
heaaaather <67219853+heaaaather@users.noreply.github.com>
2024-09-26 02:23:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def decode_image_from_base64(image_base64):
     image = Image.open(BytesIO(image_data))
     return cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)  # convert PIL image to OpenCV format
 
-model = YOLO('../models/CustomLPR.pt')
+model = YOLO('./models/CustomLPR.pt')
 reader = easyocr.Reader(['en'])
 
 # initialize image count
